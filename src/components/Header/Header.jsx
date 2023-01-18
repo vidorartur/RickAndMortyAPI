8833144477c3aa03624644a9bdf2855c1dca75c5
Artur Vidor <90942386+vidorartur@users.noreply.github.com>
2023-01-18 17:50:53 -0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css';
 function Header() {
   return (
     <div className="header">
-      <img src="https://hyperpix.net/wp-content/uploads/2020/04/rick-and-morty-logo-font-free-download.jpg" alt="logo" />
+      <img src="https://hyperpix.net/wp-content/uploads/2020/04/rick-and-morty-logo-font-free-download-856x484.jpg" alt="logo" />
     </div>
   );
 }
